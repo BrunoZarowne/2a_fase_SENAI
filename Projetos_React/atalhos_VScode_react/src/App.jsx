648a08f372bec36e474fd36b0148b4a3f1e7f919
BrunoZarowne <bruno_estudante.sesisenai.org.br>
2024-08-29 17:17:14 -0300

    Exemplos de react feito na aula de hoje

---
+++ Projetos_React/atalhos_VScode_react/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Atalhos VS Code</h1>
+      <h2>Coisas Legais para facilitar suas vidas</h2>
+      <p>
+        comando emmet : ul>li*6
+      </p>
+      <ul>
+        <li>Atalho: alt + z para quebra automatica de linha</li>
+        <li>Atalho: alt + clique esquer para adicionar novos cursores na tela</li>
+        <li>Atalho: ctrl + shift + k para excluir uma linha inteira</li>
+        <li>Atalho: ctrl + para comentar/descomentar</li>
+        <li>Atalho: windows + . abre teclado de emojis</li>
+        <li>Atalho: alt + seta cima ou baixo para mover linha ou bloco</li>
+        <li>Atalho: alt + shift + seta cima ou baixo copia ou a linha o bloco</li>
+        <li>Atalho: ctrl + j ou ctrl + '</li>
+      </ul>
+      <p>Extensões:</p>
+      <ul>
+        <li>ES7 React/Redux/GraphSQL</li>
+        <li>Auto rename</li>
+        <li>Color highlight</li>
+        <li>Power Mode</li>
+        <li></li>
+      </ul>
+    </>
+  )
+}
+
+export default App
---
+++ Projetos_React/componentes_react/src/components/Cabecalho.jsx
@@ -0,0 +1,12 @@
+import './Cabecalho.css'
+import React from 'react'
+
+function Cabecalho() {
+  return (
+    <div className='Cabecalho-container'>
+      <h1>Como treinar seu dragão</h1>
+    </div>
+  )
+}
+
+export default Cabecalho
