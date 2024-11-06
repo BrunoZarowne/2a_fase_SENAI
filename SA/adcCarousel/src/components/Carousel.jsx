0ba44921de53fa6667aab012b1a34418adfe7f7d
BrunoZarowne <bruno_estudante.sesisenai.org.br>
2024-11-06 17:24:45 -0300

    começo pra fazer a partede adc novos itens no carousel

---
+++ SA/adcCarousel/src/components/Carousel.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+function Carousel() {
+
+  function adcImgCarousel(){
+    const [itensCarousel, setItensCarousel] = useState([
+    {
+      src: 'https://m.media-amazon.com/images/I/71XYOQpyo0L._AC_UF1000,1000_QL80_.jpg',
+      alt: 'capa dandadan',
+      link: 'https://github.com/BrunoZarowne'
+    }
+  ])
+  const AdcNovoItem = () => {
+    const novoItem = {
+      src
+    }
+  }
+  }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Carousel
--- teste_carossel/src/App.jsx
+++ teste_carossel/src/App.jsx
@@ -1,19 +1,60 @@
-import React, { createElement } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
 import Carousel  from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css'
   
 export default function App() {
-  function a() {
-    const newDiv = document.createElement("div")
-    const newContent = document.createTextNode("banana")
-    newDiv.appendChild(newContent)
+  
+  function DynamicCarousel() {
+    // Estado para armazenar os itens do carousel
+    const [carouselItems, setCarouselItems] = useState([
+      {
+        src: 'https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png',
+        alt: 'Image One',
+        link: 'https://github.com/BrunoZarowne/2a_fase_SENAI'
+      },
+      {
+        src: 'https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png',
+        alt: 'Image Two'
+      }
+    ]);
+  
+    // Função para adicionar um novo item ao carousel
+    const addNewItem = () => {
+      const newItem = {
+        src: '',
+        alt: Image [carouselItems.length + 1],
+        link: ''
+      };
+      setCarouselItems([...carouselItems, newItem]);
+    };
 
-    const currentDiv = document.getElementById("div1")
-    document.body.insertBefore(newDiv, currentDiv)
-  }
   return (
-    <div id='div1' className='carousel'>
+    <div>
+      <Carousel>
+        {carouselItems.map((item, index) => (
+          <Carousel.Item key={index}>
+            {item.link ? (
+              <a href={item.link}>
+                <img src={item.src} alt={item.alt} />
+              </a>
+            ) : (
+              <img src={item.src} alt={item.alt} />
+            )}
+          </Carousel.Item>
+        ))}
+      </Carousel>
+      <button onClick={addNewItem}>Adicionar Novo Item</button>
+    </div>
+  );
+  }
+}
+
+
+// https://developer.mozilla.org/pt-BR/docs/Web/API/Document/createElement
+
+// site para ter uma base pro createElement no carousel
+
+{/* <div id='div1' className='carousel'>
       <h4>React-Bootstrap Carousel Component</h4>
       <Carousel>
         <Carousel.Item>
@@ -30,12 +71,24 @@ export default function App() {
             alt="Image Two"
           />
         </Carousel.Item>
+        <Carousel.Item>
+          <a href="https://github.com/BrunoZarowne/2a_fase_SENAI">
+          <img
+            src=""
+            alt="Image Tree"
+          />
+          </a>
+        </Carousel.Item>
+        <Carousel.Item>
+          <a href="https://github.com/BrunoZarowne/2a_fase_SENAI">
+          <img
+            src=""
+            alt="Image Four"
+          />
+          </a>
+        </Carousel.Item>
       </Carousel>
-      <button onClick={a}>sasas</button>
-    </div>
-  );
-}
+      <div id='root'>
 
-// https://developer.mozilla.org/pt-BR/docs/Web/API/Document/createElement
-
-// site para ter uma base pro createElement no carousel
+      </div>
+    </div> */}
