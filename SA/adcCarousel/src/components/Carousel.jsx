c9e5781f085fe93dbed048d3d0c0898e897c7a1d
BrunoZarowne <bruno_estudante.sesisenai.org.br>
2024-11-25 17:21:45 -0300

    finalização do carousel (só falta alguns ajustes para deixar mais limpo)

--- SA/adcCarousel/src/components/Carousel.jsx
+++ SA/adcCarousel/src/components/Carousel.jsx
@@ -52,7 +52,26 @@ function Carousel() {
     titulo: 'manga'},
   ])
   const [imagensObrasAcao, setImagensObrasacao] = useState([
-    
+    {id: '1',
+    a: 'https://stackoverflow.com/questions/65590148/swiperjs-how-do-you-style-the-pagination-bullets', 
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Bananas_%28Alabama_Extension%29.jpg/1200px-Bananas_%28Alabama_Extension%29.jpg',
+    titulo: 'banana',
+    genero: 'frutas'},
+    {id: '2', 
+    image: 'https://lh4.googleusercontent.com/proxy/164-DMLRNy1eWrUvCsbkEslbqoOtis7-OJ5DPjR0qpBk3vWf4TRtzNTcpXLlUaWxhfp4_H0m6iNcsDlnlOjuwQKw6J6A2KfF1KQQb1YNr1O9wk4Vmc5lKM8MkZz-1SGR5jbLqgRNi_C0sMmdsmiXXEUnR47U0vBkWBn9aA7WhLrbo2EXiEZ2bOLqXFk5b3cOLB2E6MCc',
+    titulo: 'maça'},
+    {id: '3', 
+    image: 'https://revistacampoenegocios.com.br/wp-content/uploads/2020/09/Foto-02-2.jpg',
+    titulo: 'manga'},
+    {id: '4', 
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Bananas_%28Alabama_Extension%29.jpg/1200px-Bananas_%28Alabama_Extension%29.jpg',
+    titulo: 'banana'},
+    {id: '5', 
+    image: 'https://lh4.googleusercontent.com/proxy/164-DMLRNy1eWrUvCsbkEslbqoOtis7-OJ5DPjR0qpBk3vWf4TRtzNTcpXLlUaWxhfp4_H0m6iNcsDlnlOjuwQKw6J6A2KfF1KQQb1YNr1O9wk4Vmc5lKM8MkZz-1SGR5jbLqgRNi_C0sMmdsmiXXEUnR47U0vBkWBn9aA7WhLrbo2EXiEZ2bOLqXFk5b3cOLB2E6MCc',
+    titulo: 'maça'},
+    {id: '6', 
+    image: 'https://revistacampoenegocios.com.br/wp-content/uploads/2020/09/Foto-02-2.jpg',
+    titulo: 'manga'},
   ])
 
   const [modalIsOpen, setIsOpen] = useState(false)
@@ -111,6 +130,34 @@ function Carousel() {
         </SwiperSlide>
       ))}
      </Swiper><br />
+     <Swiper
+      slidesPerView={1}
+      pagination={{ clickable: true}}
+      navigation  
+     >
+      {imagensObrasAcao.map((item) => (
+        <SwiperSlide key={item.id}>
+          <a href={item.a}>
+            <img className='imgs' src={item.image}/>
+          </a><br />
+          <label>{item.titulo}</label>
+        </SwiperSlide>
+      ))}
+     </Swiper><br /><br />
+     <Swiper
+      slidesPerView={1}
+      pagination={{ clickable: true}}
+      navigation  
+     >
+      {imagensObrasAcao.map((item) => (
+        <SwiperSlide key={item.id}>
+          <a href={item.a}>
+            <img className='imgs' src={item.image}/>
+          </a><br />
+          <label>{item.titulo}</label>
+        </SwiperSlide>
+      ))}
+     </Swiper><br /><br />
      <Swiper
       slidesPerView={1}
       pagination={{ clickable: true}}
